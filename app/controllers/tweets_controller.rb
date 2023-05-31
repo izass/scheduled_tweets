f54c233c0917132091c57c0728132519c9dedc5f
Isaac Moreira Barbosa <izass1312@gmail.com>
2023-05-30 22:10:56 -0300

    Implement Tweets CRUD

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,48 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_tweet, only: %i[show edit update destroy]
 
-  def index; end
+  def index
+    @tweets = Current.user.tweets
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Current.user.tweets.new(tweet_params)
+
+    if @tweet.save
+      redirect_to @tweet, notice: 'Tweet Scheduled successfully'
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: 'Tweet was updated successfully'
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: 'Tweet was unscheduled'
+  end
+
+  private
+
+  def tweet_params
+    puts params[:tweet]
+    params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find_by(id: params[:id])
+  end
 end
